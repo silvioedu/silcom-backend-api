617a49579e6595c5cb917a3eae2175aacb069100
Silvio Silva <silvioedu@gmail.com.br>
2021-09-27 14:20:47 -0300

    ajuste de mensagens

--- src/main/java/com/silcom/manager/api/controller/RamoController.java
+++ src/main/java/com/silcom/manager/api/controller/RamoController.java
@@ -23,9 +23,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @RestController
 @RequestMapping("/cadastros/ramos")
 public class RamoController {
@@ -41,7 +38,6 @@ public class RamoController {
 
     @GetMapping
     public List<RamoOutputDTO> listAll() {
-        log.info("Consultando todos os ramos");
         return ramoOutputAssembler.toColletionDTO(ramoService.findAll());
     }
 
--- src/main/java/com/silcom/manager/api/dto/input/ClienteEnderecoInputDTO.java
+++ src/main/java/com/silcom/manager/api/dto/input/ClienteEnderecoInputDTO.java
@@ -1,6 +1,7 @@
 package com.silcom.manager.api.dto.input;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,23 +11,30 @@ import lombok.Setter;
 public class ClienteEnderecoInputDTO {
     
     @NotBlank
+    @Size(max=9)
     private String cep;
 
     @NotBlank
+    @Size(max=100)
     private String logradouro;
 
     @NotBlank
+    @Size(max=20)
     private String numero;
 
+    @Size(max=100)
 	private String complemento;
 
     @NotBlank
+    @Size(max=60)
     private String bairro;
 
 	@NotBlank
+    @Size(max=60)
     private String cidade;
 
     @NotBlank
+    @Size(max=2)
     private String estado;
 
     private String observacoes;
